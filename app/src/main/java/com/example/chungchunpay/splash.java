7db215a82cf4b60a20fcb26154da8f89019942d0
park-chanyoung <pcx474@gmail.com>
2019-11-04 15:41:42 +0900

    init

---
+++ app/src/main/java/com/example/chungchunpay/splash.java
@@ -0,0 +1,37 @@
+package com.example.chungchunpay;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+
+public class splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        ImageView gif = findViewById(R.id.loading_gif);
+        GlideDrawableImageViewTarget gifImage = new GlideDrawableImageViewTarget(gif);
+        Glide.with(this).load(R.drawable.loading).into(gifImage);
+
+        Loading();
+    }
+
+    void Loading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(getBaseContext(),Login_Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
