86b836d8ba94847a0cdc5f22df2366e446f4835f
park chanyoung <pcx474@gmail.com>
2019-11-12 22:28:20 +0900

    MainActivity - WalletFragment - CardStackView 구현

--- app/src/main/java/com/example/chungchunpay/activity/MapActivity.java
+++ app/src/main/java/com/example/chungchunpay/activity/MapActivity.java
@@ -1,24 +1,17 @@
 package com.example.chungchunpay.activity;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.WindowManager;
-import android.widget.CheckedTextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.chungchunpay.R;
-import com.naver.maps.map.LocationTrackingMode;
-import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
-import com.naver.maps.map.NaverMapOptions;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.util.FusedLocationSource;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MapView mapView;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
@@ -29,168 +22,123 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+
+        //현재 내 위치
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
         init();
         click();
-    }
 
+        mapView.getMapAsync(this);
+
+        //TODO : 내부 DB(SQLite)
+    }
 
     void init() {
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setDisplayShowHomeEnabled(true);
-        }
-
-        MapFragment mapFragment = (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map_fragment);
-        if(mapFragment == null){
-            mapFragment = MapFragment.newInstance(new NaverMapOptions().locationButtonEnabled(true));
-            getSupportFragmentManager().beginTransaction().add(R.id.map_fragment,mapFragment).commit();
-        }
-        mapFragment.getMapAsync(this);
-
-        locationSource = new FusedLocationSource(this,LOCATION_PERMISSION_REQUEST_CODE);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == android.R.id.home){
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(locationSource.onRequestPermissionsResult(requestCode,permissions, grantResults)){
-            return;
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    @Override
-    public void onMapReady(@NonNull NaverMap naverMap) {
-        naverMap.setLocationSource(locationSource);
-
-        CheckedTextView none = findViewById(R.id.location_tracking_mode_none);
-        CheckedTextView noFollow = findViewById(R.id.location_tracking_mode_no_follow);
-        CheckedTextView follow = findViewById(R.id.location_tracking_mode_follow);
-        CheckedTextView face = findViewById(R.id.location_tracking_mode_face);
-
-        none.setOnClickListener(v -> naverMap.setLocationTrackingMode(LocationTrackingMode.None));
-        noFollow.setOnClickListener(v -> naverMap.setLocationTrackingMode(LocationTrackingMode.NoFollow));
-        follow.setOnClickListener(v -> naverMap.setLocationTrackingMode(LocationTrackingMode.Follow));
-        face.setOnClickListener(v -> naverMap.setLocationTrackingMode(LocationTrackingMode.Face));
-
-        naverMap.addOnOptionChangeListener(() -> {
-            LocationTrackingMode mode = naverMap.getLocationTrackingMode();
-            none.setChecked(mode == LocationTrackingMode.None);
-            noFollow.setChecked(mode == LocationTrackingMode.NoFollow);
-            follow.setChecked(mode == LocationTrackingMode.Follow);
-            face.setChecked(mode == LocationTrackingMode.Face);
-
-            locationSource.setCompassEnabled(mode == LocationTrackingMode.Follow || mode == LocationTrackingMode.Face);
-        });
-
-        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
     }
 
     void click() {
 
     }
-/*
-    public class MapFragmentActivity extends FragmentActivity implements OnMapReadyCallback {
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        naverMap.setMapType(NaverMap.MapType.Basic);
+        naverMap.setLocationSource(locationSource);
 
-        @Override
-        protected void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_map);
 
-            FragmentManager fm = getSupportFragmentManager();
-            MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
-            if (mapFragment == null) {
-                mapFragment = MapFragment.newInstance();
-                fm.beginTransaction().add(R.id.map, mapFragment).commit();
+    }
+    /*
+        public class MapFragmentActivity extends FragmentActivity implements OnMapReadyCallback {
+
+            @Override
+            protected void onCreate(@Nullable Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_map);
+
+                FragmentManager fm = getSupportFragmentManager();
+                MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map_view);
+                if (mapFragment == null) {
+                    mapFragment = MapFragment.newInstance();
+                    fm.beginTransaction().add(R.id.map_view, mapFragment).commit();
+                }
+
+                mapFragment.getMapAsync(this);
+            }
+            @Override
+            public void onMapReady(@NonNull NaverMap naverMap) {
+                Marker marker = new Marker();
+
+                //네이버 지도 타입(일반, 위성, 일반+위성(hybrid)
+    //            naverMap.setMapType(NaverMap.MapType.Hybrid);
+
+                //네이버 지도 건물 레이어 그룹
+    //            naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, false);
+    //            naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, true);
+
+                //야간 모드
+    //            naverMap.setNightModeEnabled(true);
+
+                //카메라 현재 위치
+    //            CameraPosition cameraPosition = naverMap.getCameraPosition();
+
+                //지도 위치와 카메라 위치
+    //            NaverMapOptions options = new NaverMapOptions().camera(new CameraPosition(new LatLng(35.1798159, 129.0750222), 8));
+
+                //지도 아래 패딩
+                naverMap.setContentPadding(0, 0, 0, 200);
+
+                //지도 UI setting
+                UiSettings uiSettings = naverMap.getUiSettings();
+
+                //지도 UI - Compass, Location
+                uiSettings.setCompassEnabled(true);
+                uiSettings.setLocationButtonEnabled(true);
+
+                //현재 내 위치
+                naverMap.setLocationSource(locationSource);
+
+                //위치 추적
+    //            naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+                //.None : 위치 추적 안함 / .NoFollow : 위치추적과 오버레이가 움직이나, 지도는 움직이지 않음.
+                //.Follow : 위치 추적과 지도 움직임 / .Face : 위치 추적과 베링어 움직
+
+                //카메라 줌 제한
+    //            naverMap.setMinZoom(5.0);
+    //            naverMap.setMaxZoom(18.0);
+
+                //지도에 Marker 추가
+    //            marker.setPosition(new LatLng(37.5670135, 126.9783740));
+    //            marker.setMap(naverMap);
+
+                //위치 변경 이벤트
+                naverMap.addOnLocationChangeListener(new NaverMap.OnLocationChangeListener() {
+                    @Override
+                    public void onLocationChange(@NonNull Location location) {
+
+                    }
+                });
+
+                //UI click event
+                naverMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
+
+                    }
+                });
+
             }
 
-            mapFragment.getMapAsync(this);
-        }
-        @Override
-        public void onMapReady(@NonNull NaverMap naverMap) {
-            Marker marker = new Marker();
-
-            //네이버 지도 타입(일반, 위성, 일반+위성(hybrid)
-//            naverMap.setMapType(NaverMap.MapType.Hybrid);
-
-            //네이버 지도 건물 레이어 그룹
-//            naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, false);
-//            naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, true);
-
-            //야간 모드
-//            naverMap.setNightModeEnabled(true);
-
-            //카메라 현재 위치
-//            CameraPosition cameraPosition = naverMap.getCameraPosition();
-
-            //지도 위치와 카메라 위치
-//            NaverMapOptions options = new NaverMapOptions().camera(new CameraPosition(new LatLng(35.1798159, 129.0750222), 8));
-
-            //지도 아래 패딩
-            naverMap.setContentPadding(0, 0, 0, 200);
-
-            //지도 UI setting
-            UiSettings uiSettings = naverMap.getUiSettings();
-
-            //지도 UI - Compass, Location
-            uiSettings.setCompassEnabled(true);
-            uiSettings.setLocationButtonEnabled(true);
-
-            //현재 내 위치
-            naverMap.setLocationSource(locationSource);
-
-            //위치 추적
-//            naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
-            //.None : 위치 추적 안함 / .NoFollow : 위치추적과 오버레이가 움직이나, 지도는 움직이지 않음.
-            //.Follow : 위치 추적과 지도 움직임 / .Face : 위치 추적과 베링어 움직
-
-            //카메라 줌 제한
-//            naverMap.setMinZoom(5.0);
-//            naverMap.setMaxZoom(18.0);
-
-            //지도에 Marker 추가
-//            marker.setPosition(new LatLng(37.5670135, 126.9783740));
-//            marker.setMap(naverMap);
-
-            //위치 변경 이벤트
-            naverMap.addOnLocationChangeListener(new NaverMap.OnLocationChangeListener() {
-                @Override
-                public void onLocationChange(@NonNull Location location) {
-
+            @Override
+            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+                if (locationSource.onRequestPermissionsResult(
+                        requestCode, permissions, grantResults)) {
+                    return;
                 }
-            });
-
-            //UI click event
-            naverMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {
-                @Override
-                public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
-
-                }
-            });
-
-        }
-
-        @Override
-        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-            if (locationSource.onRequestPermissionsResult(
-                    requestCode, permissions, grantResults)) {
-                return;
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
             }
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }*/
+        }*/
     @Override protected void onStart() { super.onStart();mapView.onStart(); }
     @Override public void onLowMemory() { super.onLowMemory();mapView.onLowMemory(); }
     @Override protected void onDestroy() { super.onDestroy();mapView.onDestroy(); }
@@ -198,6 +146,15 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override protected void onSaveInstanceState(@NonNull Bundle outState) { super.onSaveInstanceState(outState);mapView.onSaveInstanceState(outState); }
     @Override protected void onPause() { super.onPause();mapView.onPause(); }
     @Override protected void onResume() { super.onResume();mapView.onResume(); }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
 
 }
--- app/src/main/java/com/example/chungchunpay/fragment/WalletFragment.java
+++ app/src/main/java/com/example/chungchunpay/fragment/WalletFragment.java
@@ -2,24 +2,110 @@ package com.example.chungchunpay.fragment;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.chungchunpay.R;
+import com.example.chungchunpay.TestStackAdapter;
+import com.loopeer.cardstack.AllMoveDownAnimatorAdapter;
+import com.loopeer.cardstack.CardStackView;
+import com.loopeer.cardstack.UpDownAnimatorAdapter;
+import com.loopeer.cardstack.UpDownStackAnimatorAdapter;
+
+import java.util.Arrays;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class WalletFragment extends Fragment {
+public class WalletFragment extends Fragment implements CardStackView.ItemExpendListener{
+    public static Integer[] TEST_DATAS = new Integer[]{
+            R.color.color_1,
+            R.color.color_2,
+            R.color.color_3,
+            R.color.color_4,
+            R.color.color_5
+    };
 
+    CardStackView cardStackView;
+    LinearLayout mActionButtonContainer;
+    TestStackAdapter mTestStackAdapter;
+    Button PreButton,NextButton;
 
     public WalletFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        cardStackView = view.findViewById(R.id.CardStackView);
+        mActionButtonContainer = view.findViewById(R.id.button_container);
+        PreButton = view.findViewById(R.id.PreButton);
+        NextButton = view.findViewById(R.id.NextButton);
+
+        cardStackView.setItemExpendListener(this);
+        mTestStackAdapter = new TestStackAdapter(getContext());
+        cardStackView.setAdapter(mTestStackAdapter);
+
+        PreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardStackView.pre();
+            }
+        });
+        NextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardStackView.next();
+            }
+        });
+
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        mTestStackAdapter.updateData(Arrays.asList(TEST_DATAS));
+                    }
+                }
+                , 200
+        );
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_actions, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu_all_down:
+                cardStackView.setAnimatorAdapter(new AllMoveDownAnimatorAdapter(cardStackView));
+                break;
+            case R.id.menu_up_down:
+                cardStackView.setAnimatorAdapter(new UpDownAnimatorAdapter(cardStackView));
+                break;
+            case R.id.menu_up_down_stack:
+                cardStackView.setAnimatorAdapter(new UpDownStackAnimatorAdapter(cardStackView));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,4 +114,9 @@ public class WalletFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_wallet, container, false);
     }
 
+    @Override
+    public void onItemExpend(boolean expend) {
+        mActionButtonContainer.setVisibility(expend ? View.VISIBLE : View.GONE);
+    }
 }
+
