d561a60dd90abd53ef12bdf067b00e4f2c7dde2d
park chanyoung <pcx474@gmail.com>
2019-11-28 09:50:54 +0900

    qrcode update

---
+++ app/src/main/java/com/example/chungchunpay/activity/ScanActivity.java
@@ -0,0 +1,45 @@
+package com.example.chungchunpay.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chungchunpay.CaptureForm;
+import com.example.chungchunpay.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class ScanActivity extends AppCompatActivity {
+
+    private IntentIntegrator qrScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        qrScan = new IntentIntegrator(this);
+        qrScan.setCaptureActivity(CaptureForm.class);
+        qrScan.setOrientationLocked(false); // default가 세로모드인데 휴대폰 방향에 따라 가로, 세로로 자동 변경됩니다.
+        qrScan.setPrompt("QR코드를 인식해주세요!");
+        qrScan.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                // todo
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                // todo
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
